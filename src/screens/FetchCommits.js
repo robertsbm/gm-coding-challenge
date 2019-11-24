a8321a6c704edb1d75e3dcaa1525ddb0c25fd3a9
Michael Roberts <michael.b.roberts@gm.com>
2019-11-24 13:58:19 -0600

    [Mike R] Added constructor with loading state variable

--- src/screens/FetchCommits.js
+++ src/screens/FetchCommits.js
@@ -2,6 +2,11 @@ import React from 'react';
 import {Text, StyleSheet} from 'react-native';
 
 export default class FetchCommits extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isLoading: true}
+    }
+
     render() {
         return <Text style={styles.text}>Hello World!</Text>;
     }
